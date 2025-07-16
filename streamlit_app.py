ac2a07afa024f4a84feaea811a745a98b41c1772
RayyaanTheAnayst <87814623+RayyaanTheAnayst@users.noreply.github.com>
2025-07-16 23:17:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ from snowflake.snowpark import Session
 @st.cache_resource
 def create_session():
     params = {
-        "account": st.secrets["snowflake"]["account"].lower(),
-        "user": st.secrets["snowflake"]["user"],
-        "password": st.secrets["snowflake"]["password"],
-        "role": st.secrets["snowflake"]["role"],
-        "warehouse": st.secrets["snowflake"]["warehouse"],
-        "database": st.secrets["snowflake"]["database"],
-        "schema": st.secrets["snowflake"]["schema"],
+        "account": st.secrets["conections.snowflake"]["account"].lower(),
+        "user": st.secrets["conections.snowflake"]["user"],
+        "password": st.secrets["conections.snowflake"]["password"],
+        "role": st.secrets["conections.snowflake"]["role"],
+        "warehouse": st.secrets["conections.snowflake"]["warehouse"],
+        "database": st.secrets["conections.snowflake"]["database"],
+        "schema": st.secrets["conections.snowflake"]["schema"],
     }
     st.write({k: v for k, v in params.items() if k != "password"})
     return Session.builder.configs(params).create()
