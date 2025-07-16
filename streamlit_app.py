00968587109a0543766a84f67bc847f3985f9384
RayyaanTheAnayst <87814623+RayyaanTheAnayst@users.noreply.github.com>
2025-07-16 23:19:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ from snowflake.snowpark import Session
 @st.cache_resource
 def create_session():
     params = {
-        "account": st.secrets["conections.snowflake"]["account"].lower(),
-        "user": st.secrets["conections.snowflake"]["user"],
-        "password": st.secrets["conections.snowflake"]["password"],
-        "role": st.secrets["conections.snowflake"]["role"],
-        "warehouse": st.secrets["conections.snowflake"]["warehouse"],
-        "database": st.secrets["conections.snowflake"]["database"],
-        "schema": st.secrets["conections.snowflake"]["schema"],
+    "user": st.secrets["connections"]["snowflake"]["user"],
+    "password": st.secrets["connections"]["snowflake"]["password"],
+    "account": st.secrets["connections"]["snowflake"]["account"],
+    "warehouse": st.secrets["connections"]["snowflake"]["warehouse"],
+    "database": st.secrets["connections"]["snowflake"]["database"],
+    "schema": st.secrets["connections"]["snowflake"]["schema"],
+    "role": st.secrets["connections"]["snowflake"]["role"],
     }
     st.write({k: v for k, v in params.items() if k != "password"})
     return Session.builder.configs(params).create()
